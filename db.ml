9df61fef7fc6016983bd7e75da8b3601ecb27f31
SeiichiN <billie175@gmail.com>
2019-05-20 18:35:02 +0900

    db.ml modified at Turrys

--- db.ml
+++ db.ml
@@ -30,7 +30,7 @@ open Mysql
 
 (* 接続先のユーザ名とデータベースを決めておく *)
 (* let connect () = quick_connect ~database:"p_memo" ~password:"0405" ~user:"billie" () *)
-let db = quick_connect ~database:"p_memo" ~password:"0405" ~user:"billie" ~host:"bgn-003"()
+let db = quick_connect ~database:"p_memo" ~password:"0405" ~user:"billie" ~host:"lenovo" ()
 
 (* let db = connect () *)
 
@@ -74,3 +74,4 @@ let fold db sql f init =
   in
   loop init
   
+
